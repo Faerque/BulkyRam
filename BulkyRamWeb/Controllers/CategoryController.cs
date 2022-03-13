b5aa4f52323436a2751886526b9049889693e092
Muhammad Omar Faruk <FaerUqe@hotmail.com>
2022-03-13 16:00:42 +0600

    Add project files.

---
+++ BulkyRamWeb/Controllers/CategoryController.cs
@@ -0,0 +1,116 @@
+﻿using BulkyRamWeb.Data;
+using BulkyRamWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyRamWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        // GET Method
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        // Create Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // Edit Method
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            // var categoryFromDbFirst = _db.Categories.FirstOrDefault(c => c.Id == id);
+            // var categoryFromDbFirst = _db.Categories.SingleOrDefault(c => c.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        // POST Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // Edit Method
+        public IActionResult Remove(int? id)
+        {
+
+            var categoryFromDb = _db.Categories.Find(id);
+            // var categoryFromDbFirst = _db.Categories.FirstOrDefault(c => c.Id == id);
+            // var categoryFromDbFirst = _db.Categories.SingleOrDefault(c => c.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        // POST Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Remove(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+    }
+}
---
+++ BulkyRamWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BulkyRamWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyRamWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
+}
